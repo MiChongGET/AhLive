fda442e49b6386f19f7f240d27251a257927c3fa
MiChong <1564666023@qq.com>
2017-06-14 11:46:49 +0800

    底部导航栏的实现，

--- app/src/main/java/cn/buildworld/ahlive/SplashActivity.java
+++ app/src/main/java/cn/buildworld/ahlive/SplashActivity.java
@@ -1,7 +1,12 @@
 package cn.buildworld.ahlive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,TabActivity.class));
+                finish();
+            }
+        };
+
+        timer.schedule(timerTask,2000);
     }
 }
